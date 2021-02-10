ca4711d3d59937ce271eaa9b8669370faace31b4
Mahefa <abelmahefa@gmail.com>
2021-02-10 15:46:53 +0300

    update

--- Program.cs
+++ Program.cs
@@ -12,18 +12,33 @@ namespace CsBasic {
                 return 1;
             }
 
-            int result = Operation.factorial(nombre);
-            
-            Console.WriteLine("Resultat du Factoriel : " + result);
+            int result1 = Operation.factorialVal(nombre);
+            int result2 = nombre;
+            Operation.factorialRef(ref result2);
+
+            Console.WriteLine("Resultat du Factoriel(ByVal) : " + result1);
+            Console.WriteLine("Resultat du Factoriel(ByRef) : " + result2);
             return 0;
         }
     }
 
     class Operation {
-        public static int factorial(int nombre){
+        public static void factorialRef(ref int nombre){
             if (nombre > 0){
-                return nombre * Operation.factorial(nombre - 1);
+                int pNombre = nombre;
+                nombre = nombre - 1;
+                Operation.factorialRef(ref nombre);
+                nombre = pNombre * nombre;
             }else {
+                nombre = 1;
+            }
+        }
+
+        public static int factorialVal(int nombre)
+        {
+            if (nombre > 0){
+                return nombre * Operation.factorialVal(nombre - 1);
+            }else{
                 return 1;
             }
         }
