2ac1cf4da5e97981f5c5568ad2fcc0a92cc47278
Mahefa <abelmahefa@gmail.com>
2021-02-10 15:24:29 +0300

    ajout factorial

--- Program.cs
+++ Program.cs
@@ -2,12 +2,30 @@
 
 namespace CsBasic {
     class Principal {
-        static void Main(string[] args) {
-            string nom = "World";
+        static int Main(string[] args) {
+            int nombre = -1;
             if (args.Length > 0){
-                nom = args[0];
+                nombre = Int32.Parse(args[0]);
             }
-            Console.WriteLine("Hello, " + nom);
+            if (nombre == -1) {
+                Console.WriteLine("Mauvaise argument");
+                return 1;
+            }
+
+            int result = Operation.factorial(nombre);
+            
+            Console.WriteLine("Resultat du Factoriel : " + result);
+            return 0;
+        }
+    }
+
+    class Operation {
+        public static int factorial(int nombre){
+            if (nombre > 0){
+                return nombre * Operation.factorial(nombre - 1);
+            }else {
+                return 1;
+            }
         }
     }
 }
