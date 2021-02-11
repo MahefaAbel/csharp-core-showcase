6a19ec0a1735f3667c1c23d1c9d3e848a5adc664
Mahefa Abel <abelmahefa@gmail.com>
2021-02-11 09:28:36 +0300

    Add passage par reference

--- Program.cs
+++ Program.cs
@@ -3,11 +3,38 @@
 namespace CsBasic {
     class Principal {
         static int Main(string[] args) {
+            //Principal.testArgFactorial(args);
+            Principal.testTypeMopdifier();
+            return 0;
+        }
+
+        static int testTypeMopdifier() {
+            int nombre = 1;
+
+            Console.WriteLine("Valeur nombre (passage par valeur) [Avant] : " + nombre);
+            CaseValueRef.valueTypeModifier(nombre);
+            Console.WriteLine("Valeur nombre (passage par valeur) [Apres] : " + nombre);
+            Console.WriteLine("");
+            Console.WriteLine("Valeur nombre (passage par reference) [Avant] : " + nombre);
+            CaseValueRef.refTypeModifier(ref nombre);
+            Console.WriteLine("Valeur nombre (passage par reference) [Apres] : " + nombre);
+            Console.WriteLine("");
+            Animal animal = new Animal(3);
+            Console.WriteLine("Valeur nombre (passage par reference) [Avant] : " + animal.getAge());
+            CaseValueRef.modifObject(animal);
+            Console.WriteLine("Valeur nombre (passage par reference) [Apres] : " + animal.getAge());
+
+            return 0;
+        }
+
+        static int testArgFactorial(string[] args) {
             int nombre = -1;
-            if (args.Length > 0){
+            if (args.Length > 0)
+            {
                 nombre = Int32.Parse(args[0]);
             }
-            if (nombre == -1) {
+            if (nombre == -1)
+            {
                 Console.WriteLine("Mauvaise argument");
                 return 1;
             }
@@ -22,14 +49,19 @@ namespace CsBasic {
         }
     }
 
-    class Operation {
-        public static void factorialRef(ref int nombre){
-            if (nombre > 0){
+    class Operation
+    {
+        public static void factorialRef(ref int nombre)
+        {
+            if (nombre > 0)
+            {
                 int pNombre = nombre;
                 nombre = nombre - 1;
                 Operation.factorialRef(ref nombre);
                 nombre = pNombre * nombre;
-            }else {
+            }
+            else
+            {
                 nombre = 1;
             }
         }
@@ -42,6 +74,33 @@ namespace CsBasic {
                 return 1;
             }
         }
-
     }
+    
+        class CaseValueRef{
+            public static void valueTypeModifier(int nb){
+                nb = 20;
+            }
+
+            public static void refTypeModifier(ref int nb)
+            {
+                nb = 30;
+            }
+
+            public static void modifObject(Animal animal) {
+                animal.setAge(6);
+            }
+        }
+
+        class Animal {
+            int age;
+            public Animal(int mAge) {
+                this.age = mAge;
+            }
+            public void setAge(int nAge) {
+                this.age = nAge;
+            }
+            public int getAge() {
+                return this.age;
+            }
+        }
 }
